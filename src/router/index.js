10d69067339a11b43ef22d51516040e08c2ea080
Wangseonwoo <dmb05007@naver.com>
2023-12-19 23:38:32 +0900

    vue3-router2

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '@/views/HomeView.vue';
 import AboutView from '@/views/AboutView.vue';
+import PostCreateViewVue from '@/views/posts/PostCreateView.vue';
+import PostDetailViewVue from '@/views/posts/PostDetailView.vue';
+import PostEditViewVue from '@/views/posts/PostEditView.vue';
+import postListViewVue from '@/views/posts/postListView.vue';
 
 const routes = [
   {
@@ -12,6 +16,26 @@ const routes = [
     path: '/about',
     name: 'about',
     component: AboutView,
+  },
+  {
+    path: '/posts',
+    name: 'PostList',
+    component: postListViewVue,
+  },
+  {
+    path: '/posts/create',
+    name: 'PostCreate',
+    component: PostCreateViewVue,
+  },
+  {
+    path: '/posts/:id',
+    name: 'PostDetail',
+    component: PostDetailViewVue,
+  },
+  {
+    path: '/posts/:id/edit',
+    name: 'PostEdit',
+    component: PostEditViewVue,
   },
 ];
 const router = createRouter({
---
+++ src/views/posts/postListView.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h2>게시글 목록</h2>
+    <hr class="my-4">
+    <div class="row g-3">
+      <div v-for="post in posts" :key="post.id" class="co1-4">
+        <PostItemVuew :title="post.title" :content="post.content" :createdAt="post.createdAt" @click="goPage(post.id)"></PostItemVuew>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+  import PostItemVuew from '@/components/posts/PostItem.vue';
+  import {getPosts} from '@/api/posts'
+  import { onMounted, ref } from 'vue';
+import { useRouter } from 'vue-router';
+  
+  const router = useRouter();
+  const posts = ref([]);
+  const fetchPosts = () => {
+    posts.value = getPosts();
+  }
+  onMounted(fetchPosts);
+
+  const goPage = (id) => {
+    // router.push(`/posts/${id}`);
+    router.push({name:'PostDetail', params: {id,}})
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
