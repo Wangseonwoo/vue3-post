75016bee5340482eb985900302249c866f808914
Wangseonwoo <dmb05007@naver.com>
2024-01-09 08:51:20 +0900

    vue3-plugins

--- src/main.js
+++ src/main.js
@@ -2,8 +2,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from '@/router'
+import funcPlugins from './plugins/func';
+import objPlugins from './plugins/obj';
+import person from './plugins/person';
 
-createApp(App).use(router).mount('#app');
+const app = createApp(App);
+app.use(objPlugins, {name: '왕선우'});
+app.use(funcPlugins);
+app.use(person, {name: '홍길동'});
+app.use(router);
+app.mount('#app');
 
 import 'bootstrap/dist/js/bootstrap.js';
 import 'bootstrap-icons/font/bootstrap-icons.css';
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,25 @@
 <template>
   <div>
     <h2>Home View</h2>
+    <h2>{{ $person.name }}</h2>
+    <button class="btn btn-primary" @click="person.say">click me</button>
   </div>
 </template>
 
+<script>
+export default {
+  created() {
+    // console.log(this.$person.name);
+    // this.$person.say();
+  }
+}
+</script>
 <script setup>
+import { inject } from 'vue';
 
+
+const person = inject('person');
+console.log('person.name: ', person.name);
 </script>
 
 <style lang="scss" scoped>
