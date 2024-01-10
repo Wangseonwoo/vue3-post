4c2032cca10f19fd1ce10901be59face7ec223c1
Wangseonwoo <dmb05007@naver.com>
2024-01-10 23:37:54 +0900

    vue3-Globalplugins

--- src/main.js
+++ src/main.js
@@ -2,14 +2,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from '@/router'
-import funcPlugins from './plugins/func';
-import objPlugins from './plugins/obj';
-import person from './plugins/person';
+// import funcPlugins from './plugins/func';
+// import objPlugins from './plugins/obj';
+// import person from './plugins/person';
+import globalComponents from './plugins/global-components';
 
 const app = createApp(App);
-app.use(objPlugins, {name: '왕선우'});
-app.use(funcPlugins);
-app.use(person, {name: '홍길동'});
+// app.use(objPlugins, {name: '왕선우'});
+// app.use(funcPlugins);
+// app.use(person, {name: '홍길동'});
+app.use(globalComponents);
 app.use(router);
 app.mount('#app');
 
